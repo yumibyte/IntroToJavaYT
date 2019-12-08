a8579327c69c0afd9af1cb9650710f60f617727e
Yumibyte <ashraigosa@gmail.com>
2019-12-08 12:17:50 -0800

    pow function

---
+++ src/IntroToJavaYT/PowerFunction.java
@@ -0,0 +1,14 @@
+package IntroToJavaYT;
+
+public class PowerFunction {
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        int x = 5;
+        int y = 7;
+        int z = 57;
+        double u = x / y;
+
+        System.out.println(u);
+    }
+}
